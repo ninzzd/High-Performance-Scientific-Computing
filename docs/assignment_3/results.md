886ac3cd7867bfddcbfdd06c4c4749f8b81170e0
ninzzd <ninaad.desai1212@gmail.com>
2025-11-09 23:04:13 +0530

    Assigment 3 is complete

---
+++ docs/assignment_3/results.md
@@ -0,0 +1,95 @@
+Number of Nodes = 95
+
+Number of Threads = 2
+
+Avg Time (No-OMP) (1000 Iterations) = 552.636000
+
+Avg Time (OMP) (1000 Iterations) = 316.124000
+
+---
+
+Number of Nodes = 95
+
+Number of Threads = 4
+
+Avg Time (No-OMP) (1000 Iterations) = 584.809636
+
+Avg Time (OMP) (1000 Iterations) = 208.003124
+
+---
+
+Number of Nodes = 95
+
+Number of Threads = 8
+
+Avg Time (No-OMP) (1000 Iterations) = 601.663810
+
+Avg Time (OMP) (1000 Iterations) = 245.280003
+
+---
+
+---
+
+Number of Nodes = 198
+
+Number of Threads = 2
+
+Avg Time (No-OMP) (1000 Iterations) = 6104.064000
+
+Avg Time (OMP) (1000 Iterations) = 2694.076000
+
+---
+
+Number of Nodes = 198
+
+Number of Threads = 4
+
+Avg Time (No-OMP) (1000 Iterations) = 6320.990064
+
+Avg Time (OMP) (1000 Iterations) = 1239.191076
+
+---
+
+Number of Nodes = 198
+
+Number of Threads = 8
+
+Avg Time (No-OMP) (1000 Iterations) = 6378.515990
+
+Avg Time (OMP) (1000 Iterations) = 1246.504191
+
+---
+
+---
+
+Number of Nodes = 417
+
+Number of Threads = 2
+
+Avg Time (No-OMP) (1000 Iterations) = 42359.691000
+
+Avg Time (OMP) (1000 Iterations) = 20284.314000
+
+---
+
+Number of Nodes = 417
+
+Number of Threads = 4
+
+Avg Time (No-OMP) (1000 Iterations) = 42868.548691
+
+Avg Time (OMP) (1000 Iterations) = 11152.895314
+
+---
+
+Number of Nodes = 417
+
+Number of Threads = 8
+
+Avg Time (No-OMP) (1000 Iterations) = 42232.617549
+
+Avg Time (OMP) (1000 Iterations) = 10478.118895
+
+---
+
+---
--- src/assignment_3.c
+++ src/assignment_3.c
@@ -8,10 +8,9 @@
 #include "unistd.h"
 
 int main(){
-    set_num_threads(4);
     struct timeval start, end;
-    double **a, *b, *x0, *x_noomp, *x_omp, *err;
-    int n;
+    double **a, *b, *x0, *x_noomp, *x_omp;
+    int n,num_iter = 1000;
     FILE *kmat, *fvec, *kinfo;
     fGetMat(&kmat,&fvec,&kinfo);
     getMat(&a,&b,&n,kmat,fvec,kinfo);
@@ -22,37 +21,48 @@ int main(){
     x0 = (double*)malloc(n*sizeof(double));
     x_noomp = (double*)malloc(n*sizeof(double));
     x_omp = (double*)malloc(n*sizeof(double));
-    err = (double*)malloc(n*sizeof(double));
+    //err = (double*)malloc(n*sizeof(double));
     for(int i = 0;i < n;i++){
         x0[i] = 0.0f;
     }
+    double noomp_avg,omp_avg;
+    omp_avg = noomp_avg = 0.0;
+    for(int j = 2;j <= 8;j*=2){
+        set_num_threads(j);
+        for(int iter = 1;iter <= num_iter;iter++){
+            gettimeofday(&start,NULL);
+            bicgstab(a,b,x0,n,0.000001,0,x_noomp);
+            gettimeofday(&end,NULL);
+            int t_noomp = getCPUTime(start,end,0);
+            noomp_avg += (double)t_noomp;
 
-    gettimeofday(&start,NULL);
-    int iter_bicgstab_noomp = bicgstab(a,b,x0,n,0.000001,0,x_noomp);
-    gettimeofday(&end,NULL);
-    int t_noomp = getCPUTime(start,end,0);
-
-    gettimeofday(&start,NULL);
-    int iter_bicgstab_omp = bicgstabOMP(a,b,x0,n,0.000001,0,x_omp);
-    gettimeofday(&end,NULL);
-    int t_omp = getCPUTime(start,end,0);
-
-    for(int i = 0;i < n;i++){
-        err[i] = fabs(x_omp[i] - x_noomp[i]);
+            gettimeofday(&start,NULL);
+            bicgstabOMP(a,b,x0,n,0.000001,0,x_omp);
+            gettimeofday(&end,NULL);
+            int t_omp = getCPUTime(start,end,0);
+            omp_avg += (double)t_omp;
+        }
+        noomp_avg /= (double)num_iter;
+        omp_avg /= (double)num_iter;
+        // for(int i = 0;i < n;i++){
+        //     err[i] = fabs(x_omp[i] - x_noomp[i]);
+        // }
+        // double errnrm2;
+        // dnrm2(err,n,&errnrm2);
+        printf("Number of Nodes = %d\n",n);
+        printf("Number of Threads = %d\n",get_num_threads());
+        // printf("Number of iterations (No-OMP) = %d\n",iter_bicgstab_noomp);
+        // printf("Time taken (No-OMP) = %d\n",t_noomp);
+        // printf("Number of iterations (OMP): %d\n",iter_bicgstab_omp);
+        // printf("Time taken (OMP) = %d\n",t_omp);
+        // printf("Error L2 NRM = %lf\n",errnrm2);
+        printf("Avg Time (No-OMP) (%d Iterations) = %lf\n",num_iter,noomp_avg);
+        printf("Avg Time (OMP) (%d Iterations) = %lf\n",num_iter,omp_avg);
     }
-    double errnrm2;
-    dnrm2(err,n,&errnrm2);
-    printf("Number of Threads = %d\n",get_num_threads());
-    printf("Number of iterations (No-OMP) = %d\n",iter_bicgstab_noomp);
-    printf("Time taken (No-OMP) = %d\n",t_noomp);
-    printf("Number of iterations (OMP): %d\n",iter_bicgstab_omp);
-    printf("Time taken (OMP) = %d\n",t_omp);
-    printf("Error L2 NRM = %lf\n",errnrm2);
     free(b);
     free(x0);
     free(x_noomp);
     free(x_omp);
-    free(err);
     for(int i = 0;i < n;i++){
         free(a[i]);
     }
